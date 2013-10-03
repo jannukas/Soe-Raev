c2eb07afd409d206e0367b62bfd7c2334d66b846
jannukas <jannukas@gmail.com>
2013-10-03 16:26:01 +0300

    Update mootorid.py

--- mootorid.py
+++ mootorid.py
@@ -4,16 +4,24 @@ from time import sleep
 vasak = serial.Serial('/dev/ttyACM0')
 parem = serial.Serial('/dev/ttyACM1')
 
-def soidaedasi(kiirus):
+def jookse(kiirus):
     vasak.write('sd'+str(kiirus)+'\n')
     parem.write('sd-'+str(kiirus)+'\n')
+
+def tagane(kiirus):
+    vasak.write('sd-'+str(kiirus)+'\n')
+    parem.write('sd'+str(kiirus)+'\n')
 
 def ymberpoord():
     vasak.write('sd20\n')
     parem.write('sd20\n')
+    
+def seis():
+    vasak.write('sd20\n')
+    parem.write('sd20\n')    
 ##
 ##while (True):
-##    soidaedasi(20)
+##    jookse(20)
 ##    sleep(1.5)
 ##    ymberpoord()
 
